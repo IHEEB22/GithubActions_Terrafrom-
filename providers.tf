06a588a6759483df1cad7a5031851170449fee12
iheb <iheb.hmida1000@gmail.com>
2023-03-28 08:27:10 +0100

    change azurerm version

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.40"
+      version = "= 2.0.0"
     }
     tls = {
       source  = "hashicorp/tls"
@@ -10,9 +10,4 @@ terraform {
     }
   }
   required_version = ">= 1.2.0"
-}
-
-provider "azurerm" {
-  features {}
-
 }
