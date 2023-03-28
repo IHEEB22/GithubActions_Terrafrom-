607a7133be9c4cb5d01ee4797209683f22611d76
iheb <iheb.hmida1000@gmail.com>
2023-03-28 07:14:41 +0100

    remove unused vars

--- variables.tf
+++ variables.tf
@@ -7,15 +7,3 @@ variable "resource_group_name_prefix" {
 variable "vm_size" {
 }
 
-variable "client_secret" {
-
-}
-variable "subscription_id" {
-
-}
-variable "client_id" {
-
-}
-variable "tenant_id" {
-
-}
