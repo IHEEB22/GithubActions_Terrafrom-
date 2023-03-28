382e0ef37c0ac24c2adc03a24fc80071ba71d54e
iheb <iheb.hmida1000@gmail.com>
2023-03-28 03:46:52 +0100

    First commit

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "resource_group_location" {
+}
+
+variable "resource_group_name_prefix" {
+}
+
+variable "vm_size" {
+}
+
+variable "client_secret" {
+
+}
+variable "subscription_id" {
+
+}
+variable "client_id" {
+
+}
+variable "tenant_id" {
+
+}
