f9cf3be8cf1f3f1c2d41ecc3f36462e7f9d8da4e
iheb <iheb.hmida1000@gmail.com>
2023-03-28 05:53:56 +0100

    deploy after uploading the tfvars file

--- main.tf
+++ main.tf
@@ -121,7 +121,7 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
-  size                  = var.vm_size
+  size                  = var.size
 
   os_disk {
     name                 = "myOsDisk"
