3720ea15f08d612af29f8a260f17962786406bc8
iheb <iheb.hmida1000@gmail.com>
2023-03-28 07:51:51 +0100

    remove remote backend

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
 terraform {
   required_version = ">= 1.3"
-  backend "azurerm" {
-    resource_group_name  = "kopicloud-tfstate-rg"
-    storage_account_name = "kopicloudiactest"
-    container_name       = "core-tfstate"
-    key                  = "terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "kopicloud-tfstate-rg"
+  #   storage_account_name = "kopicloudiactest"
+  #   container_name       = "core-tfstate"
+  #   key                  = "terraform.tfstate"
+  # }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
