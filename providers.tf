c3c5e9935663914d4fcff787ce42cdf6aef57137
iheb <iheb.hmida1000@gmail.com>
2023-03-28 08:18:06 +0100

    change terraform and azurerm versions

--- providers.tf
+++ providers.tf
@@ -1,28 +1,18 @@
 terraform {
-  required_version = ">= 1.3"
-  # backend "azurerm" {
-  #   resource_group_name  = "kopicloud-tfstate-rg"
-  #   storage_account_name = "kopicloudiactest"
-  #   container_name       = "core-tfstate"
-  #   key                  = "terraform.tfstate"
-  # }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
+      version = ">= 3.40"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~>4.0"
+      version = ">=3.1"
     }
   }
+  required_version = ">= 1.2.0"
 }
-
 
 provider "azurerm" {
   features {}
+
 }
