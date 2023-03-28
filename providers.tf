b5508ef8e3711cb4aa0df3a0518672ee9eeb3826
iheb <iheb.hmida1000@gmail.com>
2023-03-28 09:28:56 +0100

    saving the tfstate file on remote backend

--- providers.tf
+++ providers.tf
@@ -1,16 +1,27 @@
 terraform {
+  required_version = ">= 1.3"
+  backend "azurerm" {
+    resource_group_name  = "iheb-tfstate-rg"
+    storage_account_name = "ihebtfhru4p07f"
+    container_name       = "core-tfstate"
+    key                  = "terraform.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.0.0"
+      version = "~>2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = ">=3.1"
+      version = "~>4.0"
     }
   }
-  required_version = ">= 1.2.0"
 }
+
 
 provider "azurerm" {
   features {}
